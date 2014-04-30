498d756ef1ad4097958efae818b8cf357f074e6d
urigarti <urigarti@gmail.com>
2014-04-30 07:11:42 +0300

    Login Code

--- src/com/community/babysit/activities/LoginActivity.java
+++ src/com/community/babysit/activities/LoginActivity.java
@@ -1,255 +1,193 @@
 package com.community.babysit.activities;
 
-import com.community.babysit.R;
-import com.community.babysit.R.id;
-import com.community.babysit.R.layout;
-import com.community.babysit.R.menu;
-import com.community.babysit.R.string;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
+import java.util.List;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.community.babysit.R;
+import com.community.babysit.util.http.JSonParser;
+import com.community.babysit.utils.encryption.PasswordEncryptionService;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.KeyEvent;
-import android.view.Menu;
+import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Activity which displays a login screen to the user, offering registration as
  * well.
  */
-public class LoginActivity extends Activity {
-	/**
-	 * A dummy authentication store containing known user names and passwords.
-	 * TODO: remove after connecting to a real authentication system.
-	 */
-	private static final String[] DUMMY_CREDENTIALS = new String[] {
-			"foo@example.com:hello", "bar@example.com:world" };
+public class LoginActivity extends Activity implements OnClickListener {
+	private EditText user, pass;
+	private Button mSubmit, mRegister;
 
-	/**
-	 * The default email to populate the email field with.
-	 */
-	public static final String EXTRA_EMAIL = "com.example.android.authenticatordemo.extra.EMAIL";
+	 // Progress Dialog
+    private ProgressDialog pDialog;
 
-	/**
-	 * Keep track of the login task to ensure we can cancel it if requested.
-	 */
-	private UserLoginTask mAuthTask = null;
+    // JSON parser class
+    JSonParser jsonParser = new JSonParser();
 
-	// Values for email and password at the time of the login attempt.
-	private String mEmail;
-	private String mPassword;
+    //php login script location:
 
-	// UI references.
-	private EditText mEmailView;
-	private EditText mPasswordView;
-	private View mLoginFormView;
-	private View mLoginStatusView;
-	private TextView mLoginStatusMessageView;
+    //localhost :
+    //testing on your device
+    //put your local ip instead,  on windows, run CMD > ipconfig
+    //or in mac's terminal type ifconfig and look for the ip under en0 or en1
+   // private static final String LOGIN_URL = "http://xxx.xxx.x.x:1234/webservice/login.php";
+
+    //testing on Emulator:
+    private static final String LOGIN_URL = "http://20.0.0.6/webservice/login.php";
+
+  //testing from a real server:
+    //private static final String LOGIN_URL = "http://www.yourdomain.com/webservice/login.php";
+
+    //JSON element ids from repsonse of php script:
+    private static final String TAG_STATUS = "status";
+    private static final String TAG_MESSAGE = "message";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.activity_login);
 
-		// Set up the login form.
-		mEmail = getIntent().getStringExtra(EXTRA_EMAIL);
-		mEmailView = (EditText) findViewById(R.id.loginUsername);
-//		mEmailView.setText(mEmail);
+		//setup input fields
+		user = (EditText)findViewById(R.id.username);
+		pass = (EditText)findViewById(R.id.password);
 
-		mPasswordView = (EditText) findViewById(R.id.password);
-		mPasswordView
-				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-					@Override
-					public boolean onEditorAction(TextView textView, int id,
-							KeyEvent keyEvent) {
-						if (id == R.id.login || id == EditorInfo.IME_NULL) {
-							attemptLogin();
-							return true;
-						}
-						return false;
-					}
-				});
+		//setup buttons
+		mSubmit = (Button)findViewById(R.id.login);
+		mRegister = (Button)findViewById(R.id.register);
 
-		mLoginFormView = findViewById(R.id.login_form);
-		mLoginStatusView = findViewById(R.id.login_status);
-		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
+		//register listeners
+		mSubmit.setOnClickListener(this);
+		mRegister.setOnClickListener(this);
 
-		findViewById(R.id.sign_in_button).setOnClickListener(
-				new View.OnClickListener() {
-					@Override
-					public void onClick(View view) {
-						attemptLogin();
-					}
-				});
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
-	}
-	
-	public void setOpenRegistrationActivity(View view) {
-		Intent registerActivityIntent = new Intent(this, RegisterActivity.class);
-		registerActivityIntent.putExtra(Intent.EXTRA_TITLE, "New User Resgitration");
-		startActivity(registerActivityIntent);
-	}
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.login:
+				new AttemptLogin().execute();
+			break;
+		case R.id.register:
+				Intent i = new Intent(this, RegisterActivity.class);
+				startActivity(i);
+			break;
 
-	/**
-	 * Attempts to sign in or register the account specified by the login form.
-	 * If there are form errors (invalid email, missing fields, etc.), the
-	 * errors are presented and no actual login attempt is made.
-	 */
-	public void attemptLogin() {
-		if (mAuthTask != null) {
-			return;
-		}
-
-		// Reset errors.
-		mEmailView.setError(null);
-		mPasswordView.setError(null);
-
-		// Store values at the time of the login attempt.
-		mEmail = mEmailView.getText().toString();
-		mPassword = mPasswordView.getText().toString();
-
-		boolean cancel = false;
-		View focusView = null;
-
-		// Check for a valid password.
-		if (TextUtils.isEmpty(mPassword)) {
-			mPasswordView.setError(getString(R.string.error_field_required));
-			focusView = mPasswordView;
-			cancel = true;
-		} else if (mPassword.length() < 4) {
-			mPasswordView.setError(getString(R.string.error_invalid_password));
-			focusView = mPasswordView;
-			cancel = true;
-		}
-
-		// Check for a valid email address.
-		if (TextUtils.isEmpty(mEmail)) {
-			mEmailView.setError(getString(R.string.error_field_required));
-			focusView = mEmailView;
-			cancel = true;
-		} else if (!mEmail.contains("@")) {
-			mEmailView.setError(getString(R.string.error_invalid_email));
-			focusView = mEmailView;
-			cancel = true;
-		}
-
-		if (cancel) {
-			// There was an error; don't attempt login and focus the first
-			// form field with an error.
-			focusView.requestFocus();
-		} else {
-			// Show a progress spinner, and kick off a background task to
-			// perform the user login attempt.
-			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
-			showProgress(true);
-			mAuthTask = new UserLoginTask();
-			mAuthTask.execute((Void) null);
+		default:
+			break;
 		}
 	}
 
-	/**
-	 * Shows the progress UI and hides the login form.
-	 */
-	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-	private void showProgress(final boolean show) {
-		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-		// for very easy animations. If available, use these APIs to fade-in
-		// the progress spinner.
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-			int shortAnimTime = getResources().getInteger(
-					android.R.integer.config_shortAnimTime);
+	class AttemptLogin extends AsyncTask<String, String, String> {
 
-			mLoginStatusView.setVisibility(View.VISIBLE);
-			mLoginStatusView.animate().setDuration(shortAnimTime)
-					.alpha(show ? 1 : 0)
-					.setListener(new AnimatorListenerAdapter() {
-						@Override
-						public void onAnimationEnd(Animator animation) {
-							mLoginStatusView.setVisibility(show ? View.VISIBLE
-									: View.GONE);
-						}
-					});
+		 /**
+         * Before starting background thread Show Progress Dialog
+         * */
+		boolean failure = false;
 
-			mLoginFormView.setVisibility(View.VISIBLE);
-			mLoginFormView.animate().setDuration(shortAnimTime)
-					.alpha(show ? 0 : 1)
-					.setListener(new AnimatorListenerAdapter() {
-						@Override
-						public void onAnimationEnd(Animator animation) {
-							mLoginFormView.setVisibility(show ? View.GONE
-									: View.VISIBLE);
-						}
-					});
-		} else {
-			// The ViewPropertyAnimator APIs are not available, so simply show
-			// and hide the relevant UI components.
-			mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
-			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-		}
-	}
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(LoginActivity.this);
+            pDialog.setMessage("Attempting login...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
 
-	/**
-	 * Represents an asynchronous login/registration task used to authenticate
-	 * the user.
-	 */
-	public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 		@Override
-		protected Boolean doInBackground(Void... params) {
-			// TODO: attempt authentication against a network service.
-
+		protected String doInBackground(String... args) {
+			// TODO Auto-generated method stub
+			 // Check for success tag
+            int success;
+            String encryptedPass;
+            String username = user.getText().toString();
+            String password = pass.getText().toString();
+            String salt = "";
 			try {
-				// Simulate network access.
-				Thread.sleep(2000);
-			} catch (InterruptedException e) {
-				return false;
+				salt = PasswordEncryptionService.generateSalt().toString();
+			} catch (NoSuchAlgorithmException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
 			}
-
-			for (String credential : DUMMY_CREDENTIALS) {
-				String[] pieces = credential.split(":");
-				if (pieces[0].equals(mEmail)) {
-					// Account exists, return true if the password matches.
-					return pieces[1].equals(mPassword);
-				}
+            try {
+				encryptedPass = PasswordEncryptionService.getEncryptedPassword(password, salt.getBytes()).toString();
+			} catch (NoSuchAlgorithmException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (InvalidKeySpecException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
 			}
+            
+            try {
+                // Building Parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("username", username));
+                params.add(new BasicNameValuePair("password", password));
+                params.add(new BasicNameValuePair("type", "ADMIN"));
+                params.add(new BasicNameValuePair("pass_salt", salt));
 
-			// TODO: register the new account here.
-			return true;
+                Log.d("request!", "starting");
+                // getting product details by making HTTP request
+                JSONObject json = jsonParser.makeHttpRequest(
+                       LOGIN_URL, "POST", params);
+
+                // check your log for json response
+                Log.d("Login attempt", json.toString());
+
+                // json success tag
+                success = json.getInt(TAG_STATUS);
+                if (success == 1) {
+                	Log.d("Login Successful!", json.toString());
+//                	Intent i = new Intent(LoginActivity.this, ReadComments.class);
+//                	finish();
+//    				startActivity(i);
+                	return json.getString(TAG_MESSAGE);
+                }else{
+                	Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+                	return json.getString(TAG_MESSAGE);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
 		}
-
-		@Override
-		protected void onPostExecute(final Boolean success) {
-			mAuthTask = null;
-			showProgress(false);
-
-			if (success) {
-				finish();
-			} else {
-				mPasswordView
-						.setError(getString(R.string.error_incorrect_password));
-				mPasswordView.requestFocus();
-			}
-		}
-
-		@Override
-		protected void onCancelled() {
-			mAuthTask = null;
-			showProgress(false);
-		}
+		/**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            if (file_url != null){
+            	Toast.makeText(LoginActivity.this, file_url, Toast.LENGTH_LONG).show();
+            }
+            if(file_url.contains("logged in")) {
+            	Intent intent = new Intent(LoginActivity.this, SittersPageActivity.class);
+            	startActivity(intent);
+            }
+        }
+        
+         
 	}
 }
--- src/com/community/babysit/activities/RegisterActivity.java
+++ src/com/community/babysit/activities/RegisterActivity.java
@@ -1,73 +1,146 @@
 package com.community.babysit.activities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.community.babysit.R;
-import com.community.babysit.R.id;
-import com.community.babysit.R.layout;
-import com.community.babysit.R.menu;
+import com.community.babysit.util.http.JSonParser;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.content.Intent;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends ActionBarActivity {
+public class RegisterActivity extends ActionBarActivity implements
+		OnClickListener {
+
+	private EditText user, pass;
+	private Button mRegister;
+
+	
+
+	// JSON parser class
+	JSonParser jsonParser = new JSonParser();
+
+	// php login script
+
+	// localhost :
+	// testing on your device
+	// put your local ip instead, on windows, run CMD > ipconfig
+	// or in mac's terminal type ifconfig and look for the ip under en0 or en1
+	// private static final String LOGIN_URL =
+	// "http://xxx.xxx.x.x:1234/webservice/register.php";
+
+	// testing on Emulator:
+	private static final String REGISTER_URL = "http://127.0.0.1/webservice/register.php";
+
+	// testing from a real server:
+	// private static final String LOGIN_URL =
+	// "http://www.yourdomain.com/webservice/register.php";
+
+	// ids
+	private static final String TAG_SUCCESS = "success";
+	private static final String TAG_MESSAGE = "message";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
 
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-		
-		Intent intent = getIntent();
-		String message = intent.getStringExtra(LoginActivity.EXTRA_EMAIL);
+		user = (EditText) findViewById(R.id.username);
+		pass = (EditText) findViewById(R.id.password);
+
+		mRegister = (Button) findViewById(R.id.register);
+		mRegister.setOnClickListener(this);
+
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.register, menu);
-		return true;
+	public void onClick(View v) {
+		new CreateUser().execute();
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	class CreateUser extends AsyncTask<String, String, String> {
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
+		/**
+		 * Before starting background thread Show Progress Dialog
+		 * */
+		boolean failure = false;
+		// Progress Dialog
+		private ProgressDialog pDialog;
 
-		public PlaceholderFragment() {
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pDialog = new ProgressDialog(RegisterActivity.this);
+			pDialog.setMessage("Creating User...");
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(true);
+			pDialog.show();
 		}
 
 		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_register,
-					container, false);
-			return rootView;
+		protected String doInBackground(String... args) {
+			// TODO Auto-generated method stub
+			// Check for success tag
+			int success;
+			String username = user.getText().toString();
+			String password = pass.getText().toString();
+			try {
+				// Building Parameters
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				params.add(new BasicNameValuePair("username", username));
+				params.add(new BasicNameValuePair("password", password));
+
+				Log.d("request!", "starting");
+
+				// Posting user data to script
+				JSONObject json = jsonParser.makeHttpRequest(REGISTER_URL, "POST",
+						params);
+
+				// full json response
+				Log.d("Login attempt", json.toString());
+
+				// json success element
+				success = json.getInt(TAG_SUCCESS);
+				if (success == 1) {
+					Log.d("User Created!", json.toString());
+					finish();
+					return json.getString(TAG_MESSAGE);
+				} else {
+					Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+					return json.getString(TAG_MESSAGE);
+
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+
+		/**
+		 * After completing background task Dismiss the progress dialog
+		 * **/
+		protected void onPostExecute(String file_url) {
+			// dismiss the dialog once product deleted
+			pDialog.dismiss();
+			if (file_url != null) {
+				Toast.makeText(RegisterActivity.this, file_url,
+						Toast.LENGTH_LONG).show();
+			}
 		}
 	}
-
 }
